9893c78309e9c08360443a5b6d832d975b4e1903
troubleman96 <itslugenge96@gmail.com>
2024-11-09 18:07:09 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program{
+
+    static void Main(string [] arg){
+
+        Console.Write("Enter a number to find its Multiplication : ");
+        int numb = Convert.ToInt32(Console.ReadLine());
+
+        for(int i = 1; i < 13; i++){
+            Console.WriteLine(numb * i);
+        }
+
+    }
+}
